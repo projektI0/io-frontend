7a67a47e5d46b95b27fd045b378f7ae8453827ad
Jakub Stępień <43409196+jkbstepien@users.noreply.github.com>
2023-03-22 15:13:45 +0100

    Cpbp 26 interfejs graficzny UI ux (#3)
    * Create graphical interface (UI/UX)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import NotFound from "./components/NotFound";
+import ShoppingList from "./components/ShoppingList";
+import Home from "./components/Home";
+import Map from "./components/Map";
+import UserProfile from "./components/UserProfile";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>CD Project Blue</h1>
-    </div>
-  )
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="App font-body grid md:grid-cols-4">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/map" element={<Map />} />
+                    <Route path="/lists" element={<ShoppingList />} />
+                    <Route path="/profile" element={<UserProfile />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="md:col-span-3 flex justify-center items-center">
+            <h1 className="p-10 text-3xl text-primary">Homepage</h1>
+        </div>
+    );
+};
+
+export default Home;

8919a61335162ad59bbed9ed07358c78ccb6b8b8
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-03-26 15:52:41 +0200

    CPBP-28-Registration
    * create RegistrationForm component, LoginForm component,
    
    * AuthService
    
    * Add protected routes
    
    * protect navbar links

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import ShoppingList from "./components/ShoppingList";
 import Home from "./components/Home";
 import Map from "./components/Map";
 import UserProfile from "./components/UserProfile";
+import LoginForm from "./components/Auth/Form/LoginForm";
+import RegisterForm from "./components/Auth/Form/RegisterForm";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
     return (
@@ -13,9 +16,11 @@ const App = () => {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/map" element={<Map />} />
-                    <Route path="/lists" element={<ShoppingList />} />
-                    <Route path="/profile" element={<UserProfile />} />
+                    <Route path="/map" element={<ProtectedRoute children={<Map />}/>}/>
+                    <Route path="/lists" element={<ProtectedRoute children={<ShoppingList />}/>}/>
+                    <Route path="/profile" element={<ProtectedRoute children={<UserProfile />}/>}/>
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/register" element={<RegisterForm />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </div>
---
+++ src/components/Auth/AuthService.tsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { User, API_HEADERS } from "./types/types";
+
+const API_URL: string = import.meta.env.VITE_API_URL;
+
+export const register = (email: string, password: string) => {
+  const data = {
+    email: email, 
+    password: password
+  };
+
+  return axios
+  .post(API_URL + "register", data, { headers: API_HEADERS })
+  .then((response) => {
+    return response;
+  })
+  .catch((error) => {
+    return error.response;
+  });
+};
+
+export const login = (email: string, password: string) => {
+  const data = {
+    email: email, 
+    password: password
+  };
+
+  return axios
+    .post(API_URL + "login", data, { headers: API_HEADERS })
+    .then((response) => {
+      if (response.data.jwtToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response;
+    })
+    .catch((error) => {
+      return error.response;
+    });
+};
+
+export const logout = () => {
+  localStorage.removeItem("user");
+};
+
+export const getCurrentUser = (): User | null => {
+  const userStr = localStorage.getItem("user");
+  if (userStr) return JSON.parse(userStr);
+
+  return null;
+};
+
+// We use it while accessing private resources: ex. axios.get(API_URL + "shops", { headers: authHeader() });
+export const authHeader = () => {
+  const userStr = localStorage.getItem("user");
+  let user = null;
+  if (userStr)
+    user = JSON.parse(userStr);
+
+  if (user && user.jwtToken) {
+    return { Authorization: 'Bearer ' + user.jwtToken };
+  } else {
+    return { Authorization: '' };
+  }
+}

5f1eaf9e8318057956d3c0dfd5e89e9c8f3d6aaf
jkbstepien <devjakub.stepien@gmail.com>
2023-05-19 00:12:37 +0200

    CPBP-69 Big screen view of navbar.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className={"flex bg-gray-100 text-sm font-body justify-center py-2 text-gray-800"}>
+        <div className={"flex bg-gray-100 text-sm font-body text-primary justify-center py-2"}>
             @Made by CD Project Blue team
         </div>
     );

76e211495b6d7f7b15dab1fe1bbf67b9315df052
jkbstepien <devjakub.stepien@gmail.com>
2023-05-19 00:25:38 +0200

    CPBP-69 Centered div ( ͡° ͜ʖ ͡°) on larger screens.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,24 +8,30 @@ const Navbar = () => {
 
     const navigate: NavigateFunction = useNavigate();
     const user: User | null = getCurrentUser();
+    // TODO: Add Profile icon to big screen navbar.
 
     return (
         <nav className="bg-primary text-bright">
-            <div className="w-full flex flex-col justify-between">
+            <div className="w-full lg:w-3/4 mx-auto flex flex-col justify-between">
                 <div className="text-2xl">
                     <div className="pl-6 py-4 pb-4 flex items-center justify-between">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-12 h-12 text-secondary">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"/>
-                        </svg>
+                        <div className={"flex items-center"}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-12 h-12 text-secondary">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"/>
+                            </svg>
+                            <div className="text-xl font-bold text-gray-200 pl-4">
+                                Shopping <br /> Manager
+                            </div>
+                        </div>
                         {!isNavOpen ? (
                             <div
                                 className={"pr-10 cursor-pointer md:hidden"}
@@ -62,7 +68,7 @@ const Navbar = () => {
                                 </svg>
                             </div>
                         )}
-                        <div className={"hidden md:block md:flex"}>
+                        <div className={"hidden md:flex"}>
                             {user && user.roles.includes("USER") &&
                                 <Link
                                     to="/active-list"
@@ -99,14 +105,13 @@ const Navbar = () => {
                                         viewBox="0 0 24 24"
                                         strokeWidth={1.5}
                                         stroke="currentColor"
-                                        className="text-secondary w-8 h-8"
-                                    >
+                                        className="w-8 h-8 text-secondary">
                                         <path
                                             strokeLinecap="round"
                                             strokeLinejoin="round"
-                                            d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
-                                        />
+                                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                                     </svg>
+
                                 </Link>
                             }
                             {user && user.roles.includes("USER") &&

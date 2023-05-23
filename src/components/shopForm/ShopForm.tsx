9f54d5862aa04cbc6fbc2427ddecd711166a320b
Michał Wójcik <michwoj01@gmail.com>
2023-05-23 17:08:35 +0200

    [CPBP-75-poprawienie-tagow]
    - added dropdowns for tags

--- src/components/shopForm/ShopForm.tsx
+++ src/components/shopForm/ShopForm.tsx
@@ -1,48 +1,80 @@
-import {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {ServerResponse, ShopFormData, ShopFormValidationError} from "./types/types";
 import {validateShopFormData} from "./validation/validation";
 import axios from "axios";
 import {authHeader} from "../auth/AuthService";
 import {NavigateFunction, useNavigate} from "react-router";
+import {Tag} from "../map/types/types";
+import {useGetAllTagsQuery} from "../../api/apiProducts";
+import Multiselect from "multiselect-react-dropdown";
 
 const API_URL: string = import.meta.env.VITE_API_URL;
 
-const ShopForm = () => {  
-    const [formData, setFormData] = useState<ShopFormData>({ name: "", latitude: 0, longitude: 0, address: "" });
+const ShopForm = () => {
+    const [formData, setFormData] = useState<ShopFormData>({
+        name: "",
+        latitude: 0,
+        longitude: 0,
+        address: "",
+        tags: []
+    });
     const [formError, setFormError] = useState<Partial<ShopFormValidationError>>({});
     const [response, setResponse] = useState<ServerResponse | null>(null);
     const [showPopup, setShowPopup] = useState<boolean>(false);
+    const [localTags, setLocalTags] = useState<Tag[]>([])
+    const {data: dataTags, isSuccess: tagsIsSuccess} = useGetAllTagsQuery()
 
-    let navigate: NavigateFunction = useNavigate();
-  
+    const navigate: NavigateFunction = useNavigate();
+
+    useEffect(() => {
+        if (!tagsIsSuccess) {
+            return
+        }
+        setLocalTags(dataTags)
+    }, [tagsIsSuccess, dataTags]);
+
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = event.target;
-      setFormData((prevState) => ({
-        ...prevState,
-        [name]: value,
-      }));
+        const {name, value} = event.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
     };
-  
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
 
-      setFormError({});
-      setResponse(null);
-      
-      const validationErrors = validateShopFormData(formData);
-      if (Object.keys(validationErrors).length > 0) {
-        setFormError(validationErrors);
+    const onSelect = (selectedList: Tag[], selectedItem: any) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            tags: selectedList
+        }));
+    }
+
+    const onRemove = (selectedList: Tag[], removedItem: any) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            tags: selectedList
+        }));
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        setFormError({});
+        setResponse(null);
+
+        const validationErrors = validateShopFormData(formData);
+        if (Object.keys(validationErrors).length > 0) {
+            setFormError(validationErrors);
 
         return;
       }
-  
-      axios.post(API_URL + "/shops", formData, {
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-          "Authorization": authHeader(),
-        }
-      }).then((response) => {
+
+        axios.post(API_URL + "/shops", {...formData, tags: formData.tags.map(tag => tag.id)}, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": authHeader(),
+            }
+        }).then((response) => {
         if (response.status === 200) {
           setResponse({status: response.status, message: "Shop added successfully!"});
         } else {
@@ -51,11 +83,11 @@ const ShopForm = () => {
       }).catch((error) => {
         setResponse({status: error.response.status, message: error.response.data.message});
       }).finally(() => {
-        setShowPopup(true);
-        setFormData({ name: "", latitude: 0, longitude: 0, address: ""});
+          setShowPopup(true);
+          setFormData({name: "", latitude: 0, longitude: 0, address: "", tags: []});
       });
     };
-  
+
     const handleGoHome = (event: React.MouseEvent<HTMLButtonElement>) => {
       event.preventDefault();
 
@@ -72,42 +104,43 @@ const ShopForm = () => {
     const clear = () => {
       setShowPopup(false);
       setFormError({});
-      setResponse(null);
-      setFormData({ name: "", latitude: 0, longitude: 0, address: ""});
+        setResponse(null);
+        setFormData({name: "", latitude: 0, longitude: 0, address: "", tags: []});
     }
 
     return (
-        (showPopup && response) ? 
-          (
-            <div className="w-3/4 md:w-1/2 lg:w-1/4 mx-auto p-10 flex flex-col">
-              <p className="text-primary font-bold text-center text-xl mb-2">{response.message}</p>
-              <div className="w-1/2 mx-auto flex flex-col justify-center mt-4 cursor-pointer">
-                  <button
-                      className="mb-2 px-2 py-2 bg-primary rounded-md text-gray-100 font-semibold border-2 hover:text-primary hover:bg-white hover:border-primary hover:border-2"
-                      onClick={handleAddAnotherShop}
-                  >
-                      Add another shop
-                  </button>
-                  <button
-                      className="px-2 py-2 bg-primary rounded-md text-gray-100 font-semibold border-2 hover:text-primary hover:bg-white hover:border-primary hover:border-2"
-                      onClick={handleGoHome}
-                  >
-                      Go home
-                  </button>
-              </div>
-            </div>
-          ): 
-          ( 
-            <div className="w-4/6 md:w-1/2 lg:w-1/3 mx-auto my-6 border-secondary border-2 rounded-md font-body text-xs lg:text-lg text-primary">
-              <form
-                  className="w-full mx-auto py-4"
-                  onSubmit={handleSubmit}
-              >
-                <div className="w-5/6 mx-auto">
-                  <label
-                      htmlFor="name"
-                      className={"mb-2 text-base md:text-lg"}
-                  >
+        (showPopup && response) ?
+            (
+                <div className="w-3/4 md:w-1/2 lg:w-1/4 mx-auto p-10 flex flex-col">
+                    <p className="text-primary font-bold text-center text-xl mb-2">{response.message}</p>
+                    <div className="w-1/2 mx-auto flex flex-col justify-center mt-4 cursor-pointer">
+                        <button
+                            className="mb-2 px-2 py-2 bg-primary rounded-md text-gray-100 font-semibold border-2 hover:text-primary hover:bg-white hover:border-primary hover:border-2"
+                            onClick={handleAddAnotherShop}
+                        >
+                            Add another shop
+                        </button>
+                        <button
+                            className="px-2 py-2 bg-primary rounded-md text-gray-100 font-semibold border-2 hover:text-primary hover:bg-white hover:border-primary hover:border-2"
+                            onClick={handleGoHome}
+                        >
+                            Go home
+                        </button>
+                    </div>
+                </div>
+            ) :
+            (
+                <div
+                    className="w-4/6 md:w-1/2 lg:w-1/3 mx-auto my-6 border-secondary border-2 rounded-md font-body text-xs lg:text-lg text-primary">
+                    <form
+                        className="w-full mx-auto py-4"
+                        onSubmit={handleSubmit}
+                    >
+                        <div className="w-5/6 mx-auto">
+                            <label
+                                htmlFor="name"
+                                className={"mb-2 text-base md:text-lg"}
+                            >
                       Name
                   </label>
                   <input
@@ -121,8 +154,8 @@ const ShopForm = () => {
                       required />
                   {formError.name && <span className="text-xs text-red-500">{formError.name}</span>}
                 </div>
-    
-                <div className="w-5/6 mx-auto">
+
+                        <div className="w-5/6 mx-auto">
                   <label
                       htmlFor="latitude"
                       className={"mb-2 text-base md:text-lg"}
@@ -138,8 +171,8 @@ const ShopForm = () => {
                       required />
                   {formError.latitude && <span className="text-xs text-red-500">{formError.latitude}</span>}
                 </div>
-    
-                <div className="w-5/6 mx-auto">
+
+                        <div className="w-5/6 mx-auto">
                   <label
                       htmlFor="longitude"
                       className={"mb-2 text-base md:text-lg"}
@@ -156,8 +189,8 @@ const ShopForm = () => {
                       required />
                   {formError.longitude && <span className="text-xs text-red-500">{formError.longitude}</span>}
                 </div>
-    
-                <div className="w-5/6 mx-auto">
+
+                        <div className="w-5/6 mx-auto">
                   <label
                       htmlFor="address"
                       className={"mb-2 text-base md:text-lg"}
@@ -172,19 +205,37 @@ const ShopForm = () => {
                       value={formData.address}
                       placeholder="Bratysławska 4"
                       onChange={handleInputChange}
-                      required />
-                  {formError.address && <span className="text-xs text-red-500">{formError.address}</span>}
-                </div>
-    
-                <div className="w-1/2 md:w-1/3 mx-auto mt-2 bg-primary text-gray-100 text-center rounded-md border-2 hover:text-primary hover:bg-white hover:border-primary hover:border-2">
-                  <button
-                      className="cursor-pointer p-2 text-sm md:text-lg font-bold"
-                      type="submit"
-                  >
-                      Add new shop
-                  </button>
-                </div>
-              </form>
+                      required/>
+                            {formError.address && <span className="text-xs text-red-500">{formError.address}</span>}
+                        </div>
+                        <div className="w-5/6 mx-auto">
+                            <label
+                                htmlFor="tags"
+                                className={"mb-2 text-base md:text-lg"}
+                            >
+                                Tags
+                            </label>
+                            <Multiselect
+                                className="m-0 border-secondary border-2 rounded-md"
+                                options={localTags}
+                                selectedValues={formData.tags}
+                                displayValue="name"
+                                onSelect={onSelect}
+                                onRemove={onRemove}
+                            />
+                        </div>
+
+
+                        <div
+                            className="w-1/2 md:w-1/3 mx-auto mt-2 bg-primary text-gray-100 text-center rounded-md border-2 hover:text-primary hover:bg-white hover:border-primary hover:border-2">
+                            <button
+                                className="cursor-pointer p-2 text-sm md:text-lg font-bold"
+                                type="submit"
+                            >
+                                Add new shop
+                            </button>
+                        </div>
+                    </form>
             </div>
           )
     );

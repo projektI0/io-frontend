c81b0b95447f7f604aca68743e930043dfce88c1
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-06-17 19:54:36 +0200

    Remove search-bar

--- src/components/map/ShopsMap.tsx
+++ src/components/map/ShopsMap.tsx
@@ -48,7 +48,6 @@ const examplePathShops: Shop[] = [
 const ShopsMap = ({userLocation}: { userLocation: LatLng | null }) => {
     const activeListId = useAppSelector(state => state.lists.activeList?.id ?? -1);
     const [path, setPath] = useState<PathResponse | null>(null);
-    const [search, setSearch] = useState<string>("");
     const [showPath, setShowPath] = useState<boolean>(false);
     const [showStops, setShowStops] = useState<boolean>(false);
     const [changeTypeBtnActive, setChangeTypeBtnActive] = useState<boolean>(false);
@@ -159,11 +158,6 @@ const ShopsMap = ({userLocation}: { userLocation: LatLng | null }) => {
         // TODO: Change type of path
     };
 
-    const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const {value} = event.target;
-        setSearch(value);
-    };
-
     if (!userLocation) {
         return (
             <div className="error-message container md:col-span-4 flex flex-col">
@@ -173,18 +167,8 @@ const ShopsMap = ({userLocation}: { userLocation: LatLng | null }) => {
     }
 
     return (
-        <div className="flex flex-col items-center py-4 px-8 justify-around font-body">
-            <div className="mb-4 w-5/6 flex justify-center items-center">
-                <input
-                    type="text"
-                    id="search"
-                    name="search"
-                    value={search}
-                    placeholder="Search for a shop"
-                    className={"border-secondary border-2"}
-                    onChange={handleSearchInputChange}/>
-            </div>
-            <div className="map-stops-container lg:min-h-[50%] mb-6">
+        <div className="flex flex-col items-center py-4 px-4 justify-around font-body">
+            <div className="map-stops-container lg:min-h-[50%] mb-6 py-3">
                 <MapContainer
                     className="map"
                     style={{
